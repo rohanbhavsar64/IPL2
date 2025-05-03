3ecd3728baf91359b90aa931bafcbfe634a18673
rohanbhavsar64 <124607265+rohanbhavsar64@users.noreply.github.com>
2025-05-03 18:44:15 +0530

    Update ipl.py

--- ipl.py
+++ ipl.py
@@ -65,5 +65,5 @@ option=st.selectbox('Enter match ID',first['Match ID'].unique())
 if option is not None:
     temp_df = match_progression(first,option,pipe)
     temp_df['over']=(120-temp_df['balls_left'])/6
-    fig=px.line(temp_df['projected_score'],temp_df['over'])
+    fig=px.line(data=temp_df,x='over',y='projected_score')
     st.write(fig)
